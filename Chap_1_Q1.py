45d8e5e56b2e63a917406a221b4abccbed66984c
semibreave <91180380+semibreave@users.noreply.github.com>
2022-09-10 15:43:44 +0800

    Create Chap_1_Q1.py

---
+++ Chap_1_Q1.py
@@ -0,0 +1,11 @@
+"""1. Print a box like the one below.
+*******************
+*******************
+*******************
+*******************"""
+for rows in range(4):
+    for columns in range(19):
+
+            print("*", end='')
+
+    print()
