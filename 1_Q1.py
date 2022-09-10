d576e1da81e33aad292b938daffbe576c16f77a2
semibreave <91180380+semibreave@users.noreply.github.com>
2022-09-10 16:03:46 +0800

    Update and rename Chap_1_Q1.py to 1_Q1.py

--- 1_Q1.py
+++ 1_Q1.py
@@ -1,8 +1,10 @@
-"""1. Print a box like the one below.
+"""
+1. Print a box like the one below.
 *******************
 *******************
 *******************
-*******************"""
+*******************
+"""
 for rows in range(4):
     for columns in range(19):
 
