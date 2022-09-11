c914b1a13b46c3665c18f8a795d0aa7097b63a05
semibreave <91180380+semibreave@users.noreply.github.com>
2022-09-12 07:18:34 +0800

    Create 2_Q8.py

---
+++ 2_Q8.py
@@ -0,0 +1,11 @@
+"""
+8. Write a program that asks the user for their name and how many times to print it. The pro-
+gram should print out the user’s name the specified number of times.
+"""
+name = input("Enter name:")
+times = eval(input("Enter how many times:"))
+
+
+for i in range(times):
+    
+     print(name)
