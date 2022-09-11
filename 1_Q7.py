98f832490102196604f96f5a88accdd22a255d42
semibreave <91180380+semibreave@users.noreply.github.com>
2022-09-11 15:04:48 +0800

    Create 1_Q7.py

---
+++ 1_Q7.py
@@ -0,0 +1,10 @@
+"""
+7. Write a program that asks the user for a weight in kilograms and converts it to pounds. There
+are 2.2 pounds in a kilogram.
+"""
+
+kg = eval(input("Enter weight in kg:"))
+pound = kg * 2.2
+
+print (kg,'kilogram in pound is:',round(pound,2))
+
