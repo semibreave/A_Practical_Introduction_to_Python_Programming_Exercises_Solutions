aece136a817c5a43bd993917927f65f0b8acb154
semibreave <91180380+semibreave@users.noreply.github.com>
2022-09-11 16:14:41 +0800

    Create 2_Q4.py

---
+++ 2_Q4.py
@@ -0,0 +1,14 @@
+"""
+4. Write a program that prints out a list of the integers from 1 to 20 and their squares. The output
+should look like this:
+1 --- 1
+2 --- 4
+3 --- 9
+...
+20 --- 400
+"""
+
+for i in range(20):
+
+    i += 1
+    print(i,i ** 2,sep=' --- ')
