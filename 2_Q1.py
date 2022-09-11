4657c7ffe86a47e43b647595fc974ee2d70a54d3
semibreave <91180380+semibreave@users.noreply.github.com>
2022-09-11 16:02:32 +0800

    Create 2_Q1.py

---
+++ 2_Q1.py
@@ -0,0 +1,7 @@
+"1. Write a program that prints your name 100 times."
+
+for i in range(100):
+
+    i += 1
+    print("MY NAME is CCP ",i,'x','.',sep='')
+                                                                                                                                                                                                          
