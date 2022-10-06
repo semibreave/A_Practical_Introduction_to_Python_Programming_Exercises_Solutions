8bf2edb8790f17774c80f848aa471d5e49f421cb
semibreave <91180380+semibreave@users.noreply.github.com>
2022-10-07 07:21:08 +0800

    Create 2_2.py

---
+++ 2_2.py
@@ -0,0 +1,9 @@
+"""
+2. Write a program to fill the screen horizontally and vertically with your name. [Hint: add the
+option end='' into the print function to fill the screen horizontally.]
+"""
+
+for i in range(10000):
+ print('Gan ',end='' )
+
+  
