137e2847bd7f19fc5dd1dedafec392d198eca2b1
semibreave <91180380+semibreave@users.noreply.github.com>
2022-10-20 15:16:12 +0800

    Create 3_6.py

---
+++ 3_6.py
@@ -0,0 +1,8 @@
+"""
+6. Write a program that asks the user to enter two numbers, x and y, and computes |x−y|/x+y .
+"""
+x = eval(input('Input x:'))
+
+y = eval(input('Input y:'))
+
+print((x-y)/(x+y))
